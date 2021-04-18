893373b290337b0a32e89ebd16fe872fe7be119e
Sarthak Gambhir <sarthak3gambhir@gmail.com>
2021-04-19 01:03:05 +0530

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+# Initialize your application
+npm init
+# Web server - express
+npm i express
+# Development debugging server
+npm i nodemon --save-dev
+# Core react - react and react-dom
+npm i react react-dom --save-dev
+# Build and packing - webpack
+npm i webpack webpack-cli --save-dev
+# JSX transpiler - Babel
+npm i babel-loader @babel/core @babel/node @babel/preset-env @babel/preset-react --save-dev
+# Linting - eslint
+npm i eslint babel-eslint eslint-plugin-react eslint-plugin-react-hooks --save-dev
+# Testing - jest
+npm i jest babel-jest react-test-renderer --save-dev
