cfe475ccf03f969dc5c167384f247bb7eaf1f6de
Sarthak Gambhir <sarthak3gambhir@gmail.com>
2021-06-12 02:51:04 +0530

    Add templating and static configuration for express server

--- react-cookie-cutter/dist/server.js
+++ react-cookie-cutter/dist/server.js
@@ -4,6 +4,9 @@ const app = express()
 
 const PORT = 5000
 
+app.set("view engine", "ejs")
+app.use(express.static("static"))
+
 app.get('/', (req, res) => res.render("index"))
 
 app.listen(PORT, () => console.log(
